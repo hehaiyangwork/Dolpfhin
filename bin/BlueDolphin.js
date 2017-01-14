c80a58f42dadc4e38fb95abe6f7f4e8b1a67d544
hehaiyangwork <hehaiyangwork@qq.com>
2017-01-14 22:56:14 +0800

    add dolphin build command @0.2.2

--- bin/BlueDolphin.js
+++ bin/BlueDolphin.js
@@ -3,9 +3,9 @@
 var program = require('commander'),
     Promise = require('bluebird'),
     gs = require('../lib/GenerateStructure'),
-    download = require('../lib/DownloadFile'),
-    spawn = require('child_process').spawn,
+    df = require('../lib/DownloadFile'),
     exec = require('child_process').exec,
+    execSync = require('child_process').execSync,
     fs = Promise.promisifyAll(require('fs-extra'));
 
 var version = require('../package.json').version;
@@ -41,8 +41,8 @@ switch(comd){
 
 /**
  * 自定义异常
- * @param {[type]} code    [description]
- * @param {[type]} message [description]
+ * @param {number} code    [description]
+ * @param {string} message [description]
  */
 function MyError(code, message){
   this.code = code;
@@ -50,45 +50,61 @@ function MyError(code, message){
 }
 
 /**
- * 目录初始化
+ * 工程初始化
  * @return {[type]} [description]
  */
 function func_init(){
   var project_name = !program.args[1] == true ? "" : program.args[1];
+  if(project_name){
+    // 必须同步执行
+    var mkdir = 'mkdir -p ./' + project_name;
+    var child = execSync(mkdir, function(err, stdout, stderr) {
+        if (err) throw err;
+    });
+  }
   return fs.readdirAsync('./' + project_name)
     .then(function(files){
-    	if(files.length > 0){
-    		throw new MyError(500, 'not empty folder');
-    	}
+    	if(files.length > 0) throw new MyError(500, 'not empty folder');
     })
     .then(function(){
-      return gs(project_name);
+      // 生成工程目录结构
+      gs(project_name);
+    })
+    .then(function(){
+      git_init(project_name);
     })
     .catch(function(err){
-      console.log(err.message);
+      console.log(err);
     });
 }
 
+/**
+ * 工程构建
+ * @return {[type]} [description]
+ */
 function func_build(){
   // 2.下载依赖 3.编译打包
-  console.log("build ...");
-  // .then(function(){
-  //   return fs.existsAsync("apps");
-  // })
-  // .then(function(exists){
-  //   console.log(exists);
-  //   console.log(1);
-  // })
-  //
-  // var outs = program.without ? program.without.split(',') : [];
-  // 加载默认配置文件
-  // var config = require(process.cwd() + "/" + pname + '/bluedolphin');
-  // git_init();
-  // Promise.all([download_vendor_files(pname)])
-  //   .then(function(){
-  //     //todo:
-  //
-  //   });
+  var config;
+  return fs.accessAsync('./bluedolphin.js', fs.F_OK)
+    .catch(function(err){
+      throw new MyError(500, 'config file [bluedolphin.js] not exist');
+    })
+    .then(function(){
+      // 加载默认配置文件
+      config = require(process.cwd() + '/bluedolphin');
+    })
+    .then(function(){
+      // 下载依赖文件
+      if(!config.vendor) throw new MyError(500, 'config vendor undefined');
+      if(!config.vendor.dir) throw new MyError(500, 'config vendor dir undefined');
+      if(!config.vendor.files) throw new MyError(500, 'config vendor files error');
+      if(!(config.vendor.files instanceof Array)) throw new MyError(500, 'config vendor files must be Array');
+
+      download_vendor_files(config.vendor.dir, config.vendor.files);
+    })
+    .catch(function(err){
+        console.log(err);
+    });
 }
 
 function func_watch(){
@@ -96,23 +112,30 @@ function func_watch(){
   console.log("watch ...");
 }
 
-// 下载依赖文件
-function download_vendor_files(pname){
-
-  var vendor_files = config.vendor_files;
-  if(vendor_files.length <= 0) return ;
-
-  for(var i = 0; i< vendor_files.length; i++ ){
-    var file_dir = !pname ? '.' + config.vendor_dir : dir + config.vendor_dir;
+/**
+ * 下载依赖文件
+ * @param  {string} vendor_dir  输出目录
+ * @param  {Array} vendor_files 文件数组
+ * @return {[type]}              [description]
+ */
+function download_vendor_files(vendor_dir, vendor_files){
+  for(var i = 0; i< vendor_files.length; i++){
+    var file_dir = vendor_dir;
     if(vendor_files[i].child_dir) file_dir += vendor_files[i].child_dir;
-
-    download(vendor_files[i].url, vendor_files[i].name, file_dir);
+    df(vendor_files[i].url, vendor_files[i].name, file_dir);
   }
-
 }
 
-function git_init(){
-  var comd = 'git init && git add . && git commit -m "first blood"';
+/**
+ * git初始化
+ * @return {[type]} [description]
+ */
+function git_init(project_name){
+  var comd = '';
+  if(project_name){
+    comd += 'cd ./' + project_name + " && ";
+  }
+  comd += 'git init && git add . && git commit -m "first blood"';
   var child = exec(comd, function(err, stdout, stderr) {
       if (err) throw err;
       else console.log("init success");
